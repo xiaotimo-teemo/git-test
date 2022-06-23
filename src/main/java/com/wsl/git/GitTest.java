0d8c3b34a78cf2ac9d4f626a641b179bd9170c7e
xiaotimo-teemo <64467628+xiaotimo-teemo@users.noreply.github.com>
2022-06-23 20:38:31 +0800

    pull commit

--- src/main/java/com/wsl/git/GitTest.java
+++ src/main/java/com/wsl/git/GitTest.java
@@ -14,6 +14,10 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hello git5");
         System.out.println("hello git5");
+        
+        System.out.println("hello git7");
+        System.out.println("hello git6");
+        System.out.println("hello git5");
     }
 
 }
